f5f3f7314ec79187aa045c8c99c6f007a1278671
Akzhol <ldgeanixr@gmail.com>
2018-10-24 21:22:33 +0600

    finish left hand

--- piano_test.asm
+++ piano_test.asm
@@ -7,6 +7,12 @@ sleep:
 	li $a0, 500 # sleep duration in ms
 	syscall
 	jr $ra
+
+sleep0.4:
+	li $v0, 32 # to sleep
+	li $a0, 400 # sleep duration in ms
+	syscall
+	jr $ra
 	
 c_sharp:
 
@@ -51,7 +57,7 @@ jr $ra
 e_low:
 
 li $v0, 31 # to produce sound
-li $a0, 64 #pitch (0-127)
+li $a0, 52 #pitch (0-127)
 li $a1, 1000 #duration in milliseconds
 li $a2, 28 #instrument (0-127)
 li $a3, 100 # volume (0-127)
@@ -98,6 +104,25 @@ li $a3, 100 # volume (0-127)
 syscall
 jr $ra
 
+g_sharp_low:
+
+li $v0, 31 # to produce sound
+li $a0, 56 #pitch (0-127)
+li $a1, 1500 #duration in milliseconds
+li $a2, 28 #instrument (0-127)
+li $a3, 100 # volume (0-127)
+syscall
+jr $ra
+
+g_sharp_low_low:
+li $v0, 31 # to produce sound
+li $a0, 44 #pitch (0-127)
+li $a1, 1500 #duration in milliseconds
+li $a2, 28 #instrument (0-127)
+li $a3, 100 # volume (0-127)
+syscall
+jr $ra
+
 a:
 
 li $v0, 31 # to produce sound
@@ -117,6 +142,26 @@ li $a2, 28 #instrument (0-127)
 li $a3, 100 # volume (0-127)
 syscall
 jr $ra
+
+a_low_low:
+
+li $v0, 31 # to produce sound
+li $a0, 45 #pitch (0-127)
+li $a1, 1000 #duration in milliseconds
+li $a2, 28 #instrument (0-127)
+li $a3, 100 # volume (0-127)
+syscall
+jr $ra
+
+a_low_low_low:
+li $v0, 31 # to produce sound
+li $a0, 33 #pitch (0-127)
+li $a1, 1000 #duration in milliseconds
+li $a2, 28 #instrument (0-127)
+li $a3, 100 # volume (0-127)
+syscall
+jr $ra
+
 
 a_sharp:
 
@@ -172,7 +217,7 @@ jr $ra
 main:
 
 
-#Part 1
+#Part1R
 jal e
 jal sleep
 jal d_sharp
@@ -191,10 +236,18 @@ jal sleep
 jal b_sharp_low
 jal sleep
 jal a_low
+jal sleep0.4
+#jal sleep
+#Part1L
+jal a_low_low
 jal sleep
+jal e_low
+jal sleep
+jal a_low
 jal sleep
 
-#Part2
+
+#Part2R
 
 
 jal b_sharp_low
@@ -204,10 +257,17 @@ jal sleep
 jal a
 jal sleep
 jal bi
+jal sleep0.4
+#Part2L
+#jal sleep
+jal g_sharp_low_low
 jal sleep
+jal e_low
+jal sleep
+jal g_sharp_low
 jal sleep
 
-#Part3
+#Part3R
 
 jal e
 jal sleep
@@ -216,10 +276,17 @@ jal sleep
 jal bi
 jal sleep
 jal b_sharp
+jal sleep0.4
+#Part1L
+jal a_low_low
+jal sleep
+jal e_low
+jal sleep
+jal a_low
 jal sleep
 jal sleep
 
-#Part 1
+#Part 1R
 jal e
 jal sleep
 jal d_sharp
@@ -238,10 +305,16 @@ jal sleep
 jal b_sharp_low
 jal sleep
 jal a_low
+jal sleep0.4
+#Part1L
+jal a_low_low
 jal sleep
+jal e_low
+jal sleep
+jal a_low
 jal sleep
 
-#Part2
+#Part2R
 
 
 jal b_sharp_low
@@ -251,10 +324,17 @@ jal sleep
 jal a
 jal sleep
 jal bi
+jal sleep0.4
+#Part2L
+#jal sleep
+jal g_sharp_low_low
 jal sleep
+jal e_low
+jal sleep
+jal g_sharp_low
 jal sleep
 
-#Part4
+#Part4R
 
 jal e
 jal sleep
@@ -263,6 +343,15 @@ jal sleep
 jal bi
 jal sleep
 jal a
+jal sleep0.4
+#Part1L
+jal a_low_low
+jal sleep
+jal e_low
+jal sleep
+jal a_low
+jal sleep
+
 jal sleep
 
 
